b1d8ed8b7a3d94ecee617e561065f3172d5a0656
liutingting <liuting_ting@163.com>
2016-08-08 15:15:40 +0800

    update

--- opera_redis.py
+++ opera_redis.py
@@ -1,9 +1,9 @@
 import redis
 import time
 #sys.path.append()
-file=open('/program-file/tt/src/main/resources/pcapAnaly.txt','r')
+file=open('/program-file/tt/src/main/resources/test.txt','r')
 #file=open('/Users/admin/Downloads/pcap_analysis-master/tt/src/main/resources/test1.txt','r')
-r = redis.StrictRedis(host='localhost', port=6379, db=5)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 done=0
 count=1
 t = time.time()
--- redis_set.py
+++ redis_set.py
@@ -1,9 +1,13 @@
+#import sys
+#sys.path.append('/Redis/redis-2.6')
 import redis
 import time
 #sys.path.append()
 file=open('/program-file/tt/src/main/resources/pcapAnaly.txt','r')
+#print file.readline()
 #file=open('/Users/admin/Downloads/pcap_analysis-master/tt/src/main/resources/test1.txt','r')
-r = redis.StrictRedis(host='localhost', port=6379, db=6)
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 done=0
 t = time.time()
 while not done:
@@ -12,8 +16,11 @@ while not done:
                 #print str
                 str_split = str.split('~')
                 #r.set(str_split[0].strip('\n'), str_split[1].strip('\n'))
+                #print str_split[0].strip('\n')
+                #print str_split[1].strip('\n')
                 r.sadd(str_split[0].strip('\n'), str_split[1].strip('\n'))
         else:
                 done=1
 print time.time() - t
+
 file.close()
